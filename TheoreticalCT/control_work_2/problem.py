dc9e8a8a9a806b13d1afae58dcb1f48111a54926
Anver Gadzhiev <gadzhiev.anver@gmail.com>
2023-12-15 15:23:35 +0300

    moved file to new folder

--- TheoreticalCT/control_work_2/problem.py
+++ TheoreticalCT/control_work_2/problem.py
@@ -1,9 +1,9 @@
 import numpy as np
 
-P = np.array([[1, -1, 0], [0, 1, -1], [2, 1, 1]])
-Q = np.array([[0, 0, 1]]).T
-R = np.array([[-1, 0, -1]])
-lamdas = np.array([-4]*P.shape[0])
+P = np.array([[0, 0, 0, -1], [0, 1, 1, 0], [1, 0, 1, 0], [0, 1, 0, 0]])
+Q = np.array([[0, 0, 0, 1]]).T
+R = np.array([[0, 0, 1, 0]])
+lamdas = np.array([-1]*P.shape[0])
 
 # P_stab= np.array([[1, 0, 0, -1], [0, 1, 1, -1], [1, -1, 0, 0], [1, 0, 0, 0]])
 # Q_stab = np.array([[0, 0, 1, 0]])
